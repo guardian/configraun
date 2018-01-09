6b89ea6d218fbbb8e25442a72f1661276078cc57
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-01-09 16:38:42 +0000

    Handle missing params better

--- src/main/scala/com/gu/configraun/models/Configuration.scala
+++ src/main/scala/com/gu/configraun/models/Configuration.scala
@@ -1,19 +1,23 @@
 package com.gu.configraun.models
 
-import com.gu.configraun.Errors.{ ConfigraunError, ParamNotOfTypeError }
-
+import com.gu.configraun.Errors.{ ConfigraunError, ParamNotOfTypeError, ParamNotExistError }
 
 case class Configuration(underlying: Map[String, Param]) {
   def getAsString(key: String): Either[ConfigraunError, String] = {
     underlying.get(key) match {
       case Some(StringParam(param)) => Right(param)
-      case _ => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type String. Try calling .getAsList.", new UnsupportedOperationException))
+      case Some(ListParam(param)) => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type String. Try calling .getAsList.", new UnsupportedOperationException))
+      case None => Left(ParamNotExistError(s"Parameter with key $key is not present.", new UnsupportedOperationException))
+      case Some(a) => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type String. Type is not ${a.getClass()}.", new UnsupportedOperationException))
     }
   }
   def getAsList(key: String): Either[ConfigraunError, Seq[String]] = {
     underlying.get(key) match {
       case Some(ListParam(param)) => Right(param)
-      case _ => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type List[String]. Try calling .getAsString.", new UnsupportedOperationException))
+      case Some(StringParam(param)) => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type List[String]. Try calling .getAsString.", new UnsupportedOperationException))
+      case None => Left(ParamNotExistError(s"Parameter with key $key is not present.", new UnsupportedOperationException))
+      case Some(a) => Left(ParamNotOfTypeError(s"Parameter with key $key is not of type List[String]. Type is ${a.getClass}.", new UnsupportedOperationException))
+
     }
   }
 }
